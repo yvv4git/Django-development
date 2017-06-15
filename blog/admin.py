1e48386670ab22279b7d20bab4d6842b00da8033
yvv4git.yvv4git <yvv4git@mail.ru>
2017-06-15 17:25:12 +0300

    Create web dev Django repo

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from blog.models import Post
+
+# Register your models here.
+admin.site.register(Post)
+#admin.site.register(Option)
---
+++ projectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/yvv/site_django/projectenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
