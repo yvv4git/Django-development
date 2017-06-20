b544168b694d5dedb12518d639e7b1958f4a925b
yvv4git.yvv4git <yvv4git@mail.ru>
2017-06-20 16:48:11 +0300

    Work with view and templates

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.utils import timezone 
+from .models import Post
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, Man!")
+	#return HttpResponse("Hello, Man!")
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
